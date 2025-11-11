0839d969496fbd54a08c281a58e000e348161dbe
Steven Bradbury <scuba2150@gmail.com>
2025-11-11 19:48:31 +1100

    updates to UI

--- src/renderer/src/components/Titlebar.jsx
+++ src/renderer/src/components/Titlebar.jsx
@@ -1,10 +1,12 @@
-import icon from "../assets/app-logo-img-only.jpg"
+import icon from '../assets/app-logo-img-only.jpg'
 
-export function Titlebar({}) {
-    return (
-        <div className="titlebar flex-c">
-            <img className="app-icon" src={icon} alt="app-icon"></img>
-            <label className="app-title">HNECT Information Centre</label>
-        </div>
-    )
+export function Titlebar() {
+  return (
+    <div className="titlebar flex-c">
+      <img className="app-icon" src={icon} alt="app-icon"></img>
+      <label className="menu-option">File</label>
+      <label className="menu-option">Edit</label>
+      <label className="menu-option">Selection</label>
+    </div>
+  )
 }
